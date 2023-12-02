bcb2cb599a2a0f237df757bedc139436ef981920
PYDONG69 <emmanuelnuswantoro@gmail.com>
2023-12-02 21:45:52 +0700

    feat(authenticate):adding login and register(DEV-14)

--- app/src/main/java/com/example/teamedup/authentication/RegisterFragment.kt
+++ app/src/main/java/com/example/teamedup/authentication/RegisterFragment.kt
@@ -1,37 +1,30 @@
 package com.example.teamedup.authentication
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.teamedup.R
-import com.example.teamedup.databinding.FragmentLoginBinding
+import com.example.teamedup.databinding.FragmentRegisterBinding
 
-class LoginFragment : Fragment() {
-    private lateinit var _binding : FragmentLoginBinding
+class RegisterFragment : Fragment() {
+    private lateinit var _binding : FragmentRegisterBinding
     private val binding get() = _binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentLoginBinding.inflate(layoutInflater,container,false)
+        _binding = FragmentRegisterBinding.inflate(layoutInflater,container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupOtherView()
-    }
-
-    private fun setupOtherView(){
         with(binding){
             toolbar.btnCreate.visibility = View.GONE
-            toolbar.tvToolbarTitle.text = "Back"
-            tvSignUp.setOnClickListener {
-                findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
-            }
+            toolbar.tvToolbarTitle.text = "Login"
         }
     }
+
 }
