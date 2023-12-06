6a1b16873700ff3df219048f31458b3a1ba5d230
PYDONG69 <emmanuelnuswantoro@gmail.com>
2023-12-06 22:39:18 +0700

    feat(tournament):adding create tournament view and navigation(DEV-24)

---
+++ app/src/main/java/com/example/teamedup/home/competition/createCompetition/CreateCompetitionFragment.kt
@@ -0,0 +1,27 @@
+package com.example.teamedup.home.competition.createCompetition
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.teamedup.R
+import com.example.teamedup.databinding.FragmentCreateCompetitionBinding
+
+class CreateCompetitionFragment : Fragment() {
+    private lateinit var _binding : FragmentCreateCompetitionBinding
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCreateCompetitionBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
