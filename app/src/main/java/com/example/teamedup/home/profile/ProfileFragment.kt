a0b4d515ad1ac798e95b9235053b0e85d36632e6
PYDONG69 <emmanuelnuswantoro@gmail.com>
2023-12-09 10:14:28 +0700

    feat(profile):adding profile view and the navigation(DEV-25)

---
+++ app/src/main/java/com/example/teamedup/home/profile/ProfileFragment.kt
@@ -0,0 +1,23 @@
+package com.example.teamedup.home.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.teamedup.R
+import com.example.teamedup.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+   private lateinit var _binding : FragmentProfileBinding
+   private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
