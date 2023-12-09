fa3f55cd10d9b26b2c730b0de7dd7c4cbf698e44
PYDONG69 <emmanuelnuswantoro@gmail.com>
2023-12-09 10:19:49 +0700

    fix(tournament):adding participated tournament view and the navigation(DEV-27)

---
+++ app/src/main/java/com/example/teamedup/home/clamaible/ParticipatedTournamentFragment.kt
@@ -0,0 +1,23 @@
+package com.example.teamedup.home.clamaible
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.teamedup.databinding.FragmentClaimablePrizeBinding
+
+
+class ParticipatedTournamentFragment : Fragment() {
+    private lateinit var _binding : FragmentClaimablePrizeBinding
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentClaimablePrizeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
